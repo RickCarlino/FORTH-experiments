e416c1dbf22cffa84dfd3e21c3500f4103e66b11
Rick Carlino <rick.carlino@gmail.com>
2016-01-02 14:33:21 -0600

    [STABLE] Functional bit-slice word

--- twiddle.fs
+++ twiddle.fs
@@ -1,16 +1,35 @@
-: ** ( n m -- n^m )
+: ** \ ( n m -- n^m )
   1 swap  0 ?do over * loop  nip ;
 
-: bits-wide ( n-bit-width -- u )
-  2 swap ** 1 - ;
+: bits-wide \ ( n-bit-width -- u )
+  #2 swap ** #1 - ;
 
-: minus-8 ( u-length -- u-minus-8 )
-    8 swap - ;
+: truncate-to-one-byte
+  #8 bits-wide and ;
 
-: clear-left ( u-target u-length -- u )
-  minus-8 lshift ;
+: push-left \ ( u-target u-times -- u )
+  lshift truncate-to-one-byte ;
 
-: clear-right ( u-target u-length -- u )
-  dup rot swap minus-8 rshift swap bits-wide and ;
+: push-right \ ( u-target u-length -- u )
+  rshift truncate-to-one-byte ;
 
 2 base !
+cr
+
+: bit-slice { target left right -- n-slice }
+  right target left push-left swap push-right
+;
+
+
+00101000 #2 #5 bit-slice .
+00101001 #2 #5 bit-slice .
+00101010 #2 #5 bit-slice .
+00101011 #2 #5 bit-slice .
+00101100 #2 #5 bit-slice .
+00101101 #2 #5 bit-slice .
+00101111 #2 #5 bit-slice .
+01101110 #2 #5 bit-slice .
+10101010 #2 #5 bit-slice .
+11101111 #2 #5 bit-slice .
+
+bye
